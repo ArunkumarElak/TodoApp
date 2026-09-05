fe0e911593257aa189fa6609cbbb229d65395945
Arunkumar L <83343824+ArunkumarElak@users.noreply.github.com>
2026-09-05 14:44:51 +0530

    Update API URL in TodoAdd component

--- client/src/TodoAdd.jsx
+++ client/src/TodoAdd.jsx
@@ -18,7 +18,7 @@ const styles = {
     }
 }
 
-const apiUrl = "http://100.55.73.218:5000"
+const apiUrl = "http://44.207.7.15:5000"
 
 export const TodoList = () => {
     const [title, setTitle] = useState("")
